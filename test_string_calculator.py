426a392d33354a6372d2a681530a12bb0831f8e3
krishnapawankar <krishnapawankar@yahoo.co.in>
2025-02-06 01:01:18 +0530

    Failing test with Failed: DID NOT RAISE <class 'Exception'>

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -33,3 +33,11 @@ def test_newline_delimiter():
 def test_custom_delimiter_single_char():
     calculator = StringCalculator()
     assert calculator.add("//;\n1;2") == 3
+
+
+def test_negative_numbers_throw_exception():
+    calculator = StringCalculator()
+    with pytest.raises(Exception) as ex:
+        calculator.add("1,-2,3")
+    assert "Negatives not allowed:" in str(ex.value)
+    assert "-2" in str(ex.value)
