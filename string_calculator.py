9c7b37df94e84ad9cc17432053c26d8af9388a76
krishnapawankar <krishnapawankar@yahoo.co.in>
2025-02-06 00:58:49 +0530

    Support single-character custom delimiter.

--- string_calculator.py
+++ string_calculator.py
@@ -5,8 +5,16 @@ class StringCalculator:
         if numbers == "":
             return 0
 
-        # Replace newline with comma, then split on comma
-        numbers = numbers.replace("\n", ",")
-        parts = numbers.split(",")
+        delimiter = ","
+        # Check if we have a custom delimiter
+        if numbers.startswith("//"):
+            # e.g. "//;\n1;2"
+            # 1) Extract custom delimiter after '//'
+            delimiter = numbers[2]
+            # 2) The numbers start after the newline
+            numbers = numbers.split("\n", 1)[1]  # everything after the first newline
+        # Now we can replace newlines with our "delimiter" as well
+        numbers = numbers.replace("\n", delimiter)
+        parts = numbers.split(delimiter)
 
         return sum(int(n) for n in parts if n)
