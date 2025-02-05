c085246ffadd9737b473e69d7c82c4bed814ba45
krishnapawankar <krishnapawankar@yahoo.co.in>
2025-02-06 00:41:55 +0530

    Failing test as add(1) currently returns 0

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -7,3 +7,8 @@ from string_calculator import StringCalculator
 def test_empty_string_returns_0():
     calculator = StringCalculator()
     assert calculator.add("") == 0
+
+
+def test_single_number_returns_value():
+    calculator = StringCalculator()
+    assert calculator.add("1") == 1
