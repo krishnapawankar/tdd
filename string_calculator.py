a49b48bc5e265a27215426f31e5483cf759498b8
krishnapawankar <krishnapawankar@yahoo.co.in>
2025-02-06 00:55:23 +0530

    Allow newlines as valid delimiters.

--- string_calculator.py
+++ string_calculator.py
@@ -4,8 +4,9 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if numbers == "":
             return 0
-        if "," in numbers:
-            nums = numbers.split(",")
-            return sum(int(n) for n in nums)
-        else:
-            return int(numbers)     # If there's only one number, convert and return
+
+        # Replace newline with comma, then split on comma
+        numbers = numbers.replace("\n", ",")
+        parts = numbers.split(",")
+
+        return sum(int(n) for n in parts if n)
