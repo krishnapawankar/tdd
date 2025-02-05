92e360ba705500831429791e3012a736fa47d346
krishnapawankar <krishnapawankar@yahoo.co.in>
2025-02-06 01:31:16 +0530

    Support multiple delimiters with length longer than one char.

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -73,3 +73,8 @@ def test_delimiter_of_any_length():
 def test_multiple_delimiters():
     calculator = StringCalculator()
     assert calculator.add("//[*][%]\n1*2%3") == 6
+
+
+def test_multiple_long_delimiters():
+    calculator = StringCalculator()
+    assert calculator.add("//[**][%%]\n1**2%%3") == 6
