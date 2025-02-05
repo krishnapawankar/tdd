72898fd4200047af531e5c86bcd565bf2932c3a2
krishnapawankar <krishnapawankar@yahoo.co.in>
2025-02-06 00:48:25 +0530

    Support two comma-delimited numbers.

--- string_calculator.py
+++ string_calculator.py
@@ -4,4 +4,8 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if numbers == "":
             return 0
-        return int(numbers)  # If there's only one number, convert and return
+        if "," in numbers:
+            nums = numbers.split(",")
+            return sum(int(n) for n in nums)
+        else:
+            return int(numbers)     # If there's only one number, convert and return
